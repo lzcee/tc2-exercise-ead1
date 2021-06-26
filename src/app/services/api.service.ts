e04b3f24fb3349c5fdc26add536bd1ea1ecd3b94
Letícia Cellurale <leticia.cellurale@gmail.com>
2021-06-26 17:34:43 -0300

    :bug: changed api url to https

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -9,7 +9,7 @@ import { Observable } from 'rxjs';
 export class ApiService {
   constructor(private http: HttpClient) {}
 
-  baseUrl = 'http://leticia-cellurale-ead-backend.herokuapp.com/api';
+  baseUrl = 'https://leticia-cellurale-ead-backend.herokuapp.com/api';
 
   getProducts(): Observable<Product[]> {
     return this.http.get<Product[]>(`${this.baseUrl}/produtos`);
